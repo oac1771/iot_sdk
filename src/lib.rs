80db4b5c447bb29756ed13935b43cb2c181063ac
Omar Carey <omarcarey@Omars-MacBook-Pro-2.local>
2026-08-25 17:57:40 -0700

    expose valuenotification

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 pub mod central;
 
-pub use btleplug::{platform::Peripheral as PlatformPeripheral, api::{Characteristic, Peripheral, CharPropFlags}};
+pub use btleplug::{platform::Peripheral as PlatformPeripheral, api::{Characteristic, Peripheral, CharPropFlags, ValueNotification}};
 pub use uuid::Uuid;
